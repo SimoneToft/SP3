8a434d14481c812a60a98b18d5616943206842b0
SimoneToft <63708995+SimoneToft@users.noreply.github.com>
2022-11-03 17:47:11 +0100

    Update startMenu.java

--- SP3/src/startMenu.java
+++ SP3/src/startMenu.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class startMenu {
+    public User currentUser;
     private ArrayList<User> users = new ArrayList<>();
     public FileIO fileIO = new FileIO();
     public boolean runLogin(String username, String password){
@@ -22,6 +24,7 @@ public class startMenu {
          for (String s : data) {
              String[] values = s.replaceAll(" ","").split(",");
              if(username.equals(values[0])&&password.equals(values[1])){
+                 currentUser = users.get(Arrays.asList(values).indexOf(username));
                  return true;
              }
          }
@@ -29,3 +32,4 @@ public class startMenu {
      }
 
 }
+
