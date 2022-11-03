868355c988c5fec3bcca9c3c12bee40cb0be003a
SimoneToft <63708995+SimoneToft@users.noreply.github.com>
2022-11-03 16:42:48 +0100

    Update startMenu.java

--- SP3/src/startMenu.java
+++ SP3/src/startMenu.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class startMenu {
+    private ArrayList<User> users = new ArrayList<>();
+    public FileIO fileIO = new FileIO();
+    public boolean runLogin(String username, String password){
+        ArrayList<String> userdata = fileIO.readUserData();
+        this.createUsers(userdata);
+        return this.loginCheck(userdata,username,password);
+    }
+    public void addUser(String username, String password){
+        FileIO.newUser(username,password);
+    }
+    private void createUsers(ArrayList<String> data){
+        for (String s : data) {
+            String[] values = s.replaceAll(" ","").split(",");
+            User p = new User(values[0], values[1]);
+            users.add(p);
+        }
+    }
+     private boolean loginCheck(ArrayList<String> data, String username, String password){
+         for (String s : data) {
+             String[] values = s.replaceAll(" ","").split(",");
+             if(username.equals(values[0])&&password.equals(values[1])){
+                 return true;
+             }
+         }
+         return false;
+     }
 
 }
