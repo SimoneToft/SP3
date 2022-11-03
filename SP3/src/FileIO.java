c08e9e70e823ac2a82b6a24a1b2cad0fc3a3d9a3
SimoneToft <63708995+SimoneToft@users.noreply.github.com>
2022-11-03 16:42:10 +0100

    Update FileIO.java

--- SP3/src/FileIO.java
+++ SP3/src/FileIO.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+
 public class FileIO {
+    public ArrayList<String> readUserData(){
+        File userfile = new File("Data/user.csv");
+        ArrayList<String> userdata = new ArrayList<>();
+        try{
+            Scanner input = new Scanner(userfile);
+            input.nextLine();
+            while (input.hasNextLine()){
+                userdata.add(input.nextLine());
+            }
+        } catch (FileNotFoundException e){
+            userdata=null;
+        }
+        return userdata;
+    }
+    public static void newUser(String username, String password){
+        try {
+            FileWriter writer = new FileWriter("Data/user.csv", true);
+            writer.write("\n"+username+","+password);
+            writer.close();
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+
+    }
 }
+
