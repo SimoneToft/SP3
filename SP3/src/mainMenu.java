0c7d01e076a0b8b90c73b37d9f4b40f615dd01fa
SimoneToft <63708995+SimoneToft@users.noreply.github.com>
2022-11-04 16:20:43 +0100

    Update mainMenu.java

--- SP3/src/mainMenu.java
+++ SP3/src/mainMenu.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-
 public class mainMenu {
+    public static int mediaAmount;
+    public static boolean seriesDisplayed=false;
+    public static boolean moviesDisplayed=false;
     public static void runMainMenu() {
+        createMedia.createAllMedia();
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("1. View all movies\n2. Search\n3. Categories\n4. Watched\n5. Saved");
+        System.out.println("1. View all movies\n2. View all series\n3. Search\n4. Categories\n5. Watched\n6. Saved");
         int intInputMain;
         intInputMain = keyboard.nextInt();
         if(intInputMain == 1){
-            System.out.println("1");
+            mediaAmount=createMedia.allMovies();
+            moviesDisplayed=true;
+            mediaSelector();
         }
         else if(intInputMain == 2){
-            System.out.println("2");
+            mediaAmount=createMedia.allSeries();
+            seriesDisplayed=true;
+            mediaSelector();
         }
         else if(intInputMain == 3){
             System.out.println("3");
@@ -22,5 +29,11 @@ public class mainMenu {
             System.out.println("5");
         }
     }
+    public static void mediaSelector(){
+        Scanner keyboard = new Scanner(System.in);
+        int choice=-1+keyboard.nextInt();
+        if(choice>=0&&choice<=mediaAmount){
+            System.out.println(createMedia.getTitle(moviesDisplayed,seriesDisplayed,choice)+" is now playing...");
+        }
+    }
 }
-
