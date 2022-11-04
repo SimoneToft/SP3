6b84613067c60d62ac62e1b14674e97e1bf4736c
SimoneToft <63708995+SimoneToft@users.noreply.github.com>
2022-11-04 16:22:04 +0100

    Update startMenu.java

--- SP3/src/startMenu.java
+++ SP3/src/startMenu.java
@@ -5,13 +5,28 @@ public class startMenu {
     public User currentUser;
     private ArrayList<User> users = new ArrayList<>();
     public FileIO fileIO = new FileIO();
+    ArrayList<String> userdata = fileIO.readUserData();
     public boolean runLogin(String username, String password){
-        ArrayList<String> userdata = fileIO.readUserData();
         this.createUsers(userdata);
         return this.loginCheck(userdata,username,password);
     }
     public void addUser(String username, String password){
-        FileIO.newUser(username,password);
+        if(this.usernameCheck(userdata, username)){
+            System.out.println("This username is taken");
+        }
+        else {
+            FileIO.newUser(username, password);
+            System.out.println("New user created!");
+        }
+    }
+    private boolean usernameCheck(ArrayList<String> data, String username){
+        for (String s : data) {
+            String[] values = s.replaceAll(" ","").split(",");
+            if(username.equals(values[0])){
+                return true;
+            }
+        }
+        return false;
     }
     private void createUsers(ArrayList<String> data){
         for (String s : data) {
@@ -30,6 +45,5 @@ public class startMenu {
          }
          return false;
      }
-
 }
 
