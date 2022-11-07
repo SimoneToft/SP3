8d1dbfc02af9d159a6e7e4e55e2a207583aa83db
panksxd <113101232+panksxd@users.noreply.github.com>
2022-11-07 16:15:26 +0100

    Daniels User fil

--- src/User.java
+++ src/User.java
@@ -22,53 +22,99 @@ public class User {
     public void saveMedia(boolean a, boolean b, String str){
         if(a){
             List<String> mediaList;
+            boolean alreadySaved = false;
             if(!Objects.equals(savedMovies[0], "null")){
                 mediaList = new ArrayList<String>(Arrays.asList(savedMovies));
             }
             else{
                 mediaList = new ArrayList<String>();
             }
-            mediaList.add(str);
-            savedMovies=mediaList.toArray(savedMovies);
-            startMenu.saveUsers();
+            for(String saved : savedMovies)
+            {
+                if(saved.equals(str))
+                {
+                    alreadySaved = true;
+                    break;
+                }
+            }
+            if(!alreadySaved) {
+                mediaList.add(str);
+                savedMovies = mediaList.toArray(savedMovies);
+                startMenu.saveUsers();
+
+            }
+
         }
         if (b){
             List<String> mediaList;
+            boolean alreadySaved = false;
             if(!Objects.equals(savedSeries[0], "null")){
                 mediaList = new ArrayList<String>(Arrays.asList(savedSeries));
             }
             else{
                 mediaList = new ArrayList<String>();
             }
-            mediaList.add(str);
-            savedSeries=mediaList.toArray(savedSeries);
-            startMenu.saveUsers();
+            for(String saved : savedSeries)
+            {
+                if(saved.equals(str))
+                {
+                    alreadySaved = true;
+                    break;
+                }
+            }
+            if(!alreadySaved) {
+                mediaList.add(str);
+                savedSeries = mediaList.toArray(savedSeries);
+                startMenu.saveUsers();
+            }
         }
     }
     public void watchedMedia(boolean a, boolean b, String str){
         if(a){
             List<String> mediaList;
+            boolean alreadyWatched = false;
             if(!Objects.equals(watchedMovies[0], "null")){
                 mediaList = new ArrayList<String>(Arrays.asList(watchedMovies));
             }
             else{
                 mediaList = new ArrayList<String>();
             }
-            mediaList.add(str);
-            watchedMovies=mediaList.toArray(watchedMovies);
-            startMenu.saveUsers();
+            for(String watched : watchedMovies)
+            {
+                if(watched.equals(watchedMovies))
+                {
+                    alreadyWatched = true;
+                    break;
+                }
+            }
+            if(!alreadyWatched) {
+                mediaList.add(str);
+                watchedMovies = mediaList.toArray(watchedMovies);
+                startMenu.saveUsers();
+            }
         }
         if (b){
             List<String> mediaList;
+            boolean alreadyWatched = false;
             if(!Objects.equals(watchedSeries[0], "null")){
                 mediaList = new ArrayList<String>(Arrays.asList(watchedSeries));
             }
             else{
                 mediaList = new ArrayList<String>();
             }
-            mediaList.add(str);
-            watchedSeries=mediaList.toArray(watchedSeries);
-            startMenu.saveUsers();
+            for(String watched : watchedSeries)
+            {
+                if(watched.equals(str))
+                {
+                    alreadyWatched = true;
+                    break;
+                }
+            }
+            if(!alreadyWatched) {
+                mediaList.add(str);
+                watchedSeries = mediaList.toArray(watchedSeries);
+                startMenu.saveUsers();
+            }
         }
     }
 
