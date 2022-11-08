75420545612cb90c24c40c8bc115ef5fcfa90238
Toebzy <113095884+Toebzy@users.noreply.github.com>
2022-11-08 13:54:12 +0100

    Update Series.java

--- src/Series.java
+++ src/Series.java
@@ -39,7 +39,37 @@ public class Series {
                 number++;
             }
         }
-        System.out.println("Select a movie, or press '0' to return");
+        System.out.println("Select a movie, or press ¨0¨ to return");
+        Scanner choice = new Scanner(System.in);
+        int nextChoice = choice.nextInt();
+
+        if (nextChoice == 0) {
+            mainMenu.runMainMenu();
+
+        } else if (nextChoice <= seriesSearch.size()) {
+            String movieTitle = seriesSearch.get(nextChoice - 1).getMediaTitle();
+            mainMenu.mediaPlayer(movieTitle, true, false);
+        }
+        else if (nextChoice > seriesSearch.size()||nextChoice < seriesSearch.size()){
+
+            System.out.println("-----------------------------------------");
+            System.out.println("The movie was not found, please try again");
+            System.out.println("-----------------------------------------");
+
+            titleSearchSeries(search);
+
+        } else {
+            for (Series p : createMedia.series) {
+                if (p.getMediaTitle().toLowerCase().contains(search.toLowerCase())) {
+                    seriesSearch.add(p);
+                }
+            }
+            for (Series p : seriesSearch) {
+                System.out.println(number + ". " + p);
+                number++;
+            }
+        }
+        System.out.println("Select a movie, or press ¨0¨ to return");
         Scanner choice = new Scanner(System.in);
         int nextChoice = choice.nextInt();
 
@@ -51,6 +81,7 @@ public class Series {
             mainMenu.mediaPlayer(movieTitle, true, false);
         }
     }
+
 
     @Override
     public String toString() {
